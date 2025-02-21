b8507cb6cedd2380b65577cf323aacca3bb30e1d
Roland Arsenault <roland@ccom.unh.edu>
2025-02-21 14:48:12 -0500

    Make tf able to be namespaced

--- launch/publish_state_launch.py
+++ launch/publish_state_launch.py
@@ -13,6 +13,8 @@ def generate_launch_description():
     namespace_arg = DeclareLaunchArgument(
       "namespace", default_value=TextSubstitution(text="ben")
     )
+
+    remappings = [('/tf', 'tf'), ('/tf_static', 'tf_static')]
     
     path_to_urdf = get_package_share_path('ben_description') / 'urdf' / 'ben_mesh.xacro'
     robot_state_publisher_node = Node(
@@ -24,14 +26,18 @@ def generate_launch_description():
            'robot_description': ParameterValue(
             Command(['xacro ', str(path_to_urdf)]), value_type=str
            )
-       }]
+       }],
+       remappings=remappings
     )
 
+
+
     joint_state_publisher_node = Node(
-       package='joint_state_publisher',
-       executable='joint_state_publisher',
-       name='joint_state_publisher',
-       namespace=namespace
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher',
+        namespace=namespace,
+        remappings=remappings
     )
 
     return LaunchDescription([
